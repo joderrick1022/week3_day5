e372b16a682564445d05aede4f9724beb55153df
joderrick1022 <joderrick1022>
2017-10-13 10:06:01 -0400

    created army of turtles and cats

---
+++ week3_day5/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3_day5
+{
+    class Cat
+    {
+
+        private int CatCount
+        {
+            get { return this.CatCount; }
+            set { this.CatCount = value; }
+        }
+
+        public Cat()
+        {
+
+        }
+
+        public void Warcry()
+        {
+            Console.WriteLine("week3_day5 are a army of " + CatCount);
+        }
+    }
+}
---
+++ week3_day5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3_day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Cat> catArmy = new List<Cat>();
+         
+             List <Turtles> turtlArmy = new List<Turtles>();
+
+            Random rand = new Random();
+
+
+            //Lets create our aremys
+
+            for (int i = 0; i < 100; i++)
+            {
+                if(rand.NextDouble() >= 0.5)
+                {
+                    catArmy.Add(new Cat());
+                }
+                else
+                {
+                    turtlArmy.Add(new Turtles());
+                }
+            }
+        
+        }
+    }
+}
